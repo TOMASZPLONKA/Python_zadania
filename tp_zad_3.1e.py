0248110750b1ea4e2eba88cdb94462b1a7828977
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-20 23:36:55 +0100

    Update tp_zad_3.1e.py

--- tp_zad_3.1e.py
+++ tp_zad_3.1e.py
@@ -1,18 +1,9 @@
-"""
-Stwórz następujące funkcje. Niech każda z nich przyjmuje w argument do przeliczenia i zwraca przeliczoną wartość.
-
-5. `bmi` - oblicza współczynnik BMI dla podanych dwóch parametrów: wzrostu w metrach i wagi w kg.
-
-Dla wybranych napisz też interaktywne programy, które pytają użytkownika o dane i wypisują wynik.
-
-"""
-
 wzrost = int(input('Podaj wzrost (w cm): '))
 waga = float(input('Podaj wagę (w kg): '))
-
 def bmi(waga:float,wzrost:int):
     """docstring: składnia: waga, typ danych: int,
     wzrost, typ danych: float"""
+
     BMI = round((waga/(wzrost/100) ** 2),2)
     if BMI < 18.5:
         return print(f'Twoje BMI = {BMI}. Niedobrze. Masz niedowagę. Skontaktuj się z lekarzem i dietetykiem.')
@@ -23,13 +14,13 @@ def bmi(waga:float,wzrost:int):
     else:
         return print(f'Twoje BMI = {BMI}. Jest bardzo źle! Chorujesz na otyłość. Natychmiast skonsultuj się z lekarzem.')
 
-bmi(waga,wzrost)
+bmi(87,187)
 
-print(bmi.__doc__ )
+
 print(60*'=')
 
 def test_BMI_Normal():
-    assert bmi(87,187) == 24.88
+    assert bmi(87,187) == print(f'Twoje BMI = {24.88}. Świetnie. Twoja waga jest w sam raz.')
 
 def test_BMI_Nadwaga():
-    assert bmi(110,200) == 27.5
+    assert bmi(110,200) == print(f'Twoje BMI = {27.5}. Uważaj. Masz nadwagę.')
