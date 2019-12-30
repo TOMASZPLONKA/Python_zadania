d82aaed328f9df0fb256b775f041ddf69419a638
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-30 23:33:57 +0100

    Delete zad_7.1.py

--- zad_7.1.py
+++
@@ -1,9 +0,0 @@
-napis = input("Podaj napis: ")
-samogloski = ['a','e','i','o','u'] # lista
-samogloski = ('a','e','i','o','u') # tupla
-samogloski = 'aeiou' # ciąg znaków - też zadziała
-ile_samoglosek = 0
-
-for samogloska in samogloski:
-    ile_samoglosek += napis.lower().count(samogloska)
-print(f'Liczba samogłosek w podanym napisie: {ile_samoglosek} ')
