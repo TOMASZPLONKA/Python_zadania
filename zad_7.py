93ed4810292df217fccb75db775602e3ba7fd32f
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-30 23:33:47 +0100

    Delete zad_7.py

--- zad_7.py
+++
@@ -1,6 +0,0 @@
-liczba = int(input("Podaj liczbę: "))
-
-wynik = (liczba % 2 != 0 and liczba % 3 == 0 and liczba > 10) or liczba == 7
-
-# proces wyliczania złożonego wyrażenia (takiego, które nie jest zwykłą wartością) nazywamy ewaluacją
-print(f'Wynik: {wynik}')
