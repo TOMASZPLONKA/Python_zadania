fe1e71602b62460907d83de50582166663f9c087
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-30 23:33:23 +0100

    Delete zad_8.py

--- zad_8.py
+++
@@ -1,7 +0,0 @@
-wymiar_a = int(input("Podaj wymiar a w cm: "))
-wymiar_b = int(input("Podaj wymiar b w cm: "))
-wymiar_c = int(input("Podaj wymiar c w cm: "))
-
-objetosc = wymiar_a  * wymiar_b * wymiar_c
-
-print(f' Objętość opakowania jest większa od 1 litra: {objetosc > 1000}')
