a538ba761a6fef08460b8920d036955694a4ec91
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-30 23:34:10 +0100

    Delete zad_6.py

--- zad_6.py
+++
@@ -1,5 +0,0 @@
-liczba = int(input("Podaj liczbę: "))
-
-print(f' {liczba} jest większa od 10: {liczba > 10}')
-print(f' {liczba} jest mniejsza lub równa 15: {liczba <= 15}')
-print(f' {liczba} jest podzielna przez 2: {liczba % 2 == 0}')
