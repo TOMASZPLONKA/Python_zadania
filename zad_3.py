a8af793f9bb916e3fd510070d60400843a154aa8
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2019-12-30 23:37:50 +0100

    Delete zad_3.py

--- zad_3.py
+++
@@ -1,9 +0,0 @@
-imie = 'Tomasz'
-wzrost = 187
-
-print('Imię:', imie) # 2 argumenty, przy kilku argumentach print oddziela je spracją
-print('Wzrost:', wzrost)
-
-print('Imię: ' + str (imie))
-print('Wzrost: ' + str (wzrost)) # 1 argument, przy 1 argumencie nie ma spacji
-
