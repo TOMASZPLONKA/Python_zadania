841f7f04785b17bd44ee545a0aa0a508a149e070
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2020-01-01 18:52:31 +0100

    Update tp_zad_4_11.py

--- tp_zad_4_11.py
+++ tp_zad_4_11.py
@@ -1,6 +1,8 @@
 """
 # Zadanie #11
-Zaimplementuj klasy odpowiedzialne za tworzenie dokumentów w składni MarkDown. Stwórz klasę bazową `Element` zawierającą podstawową implementację metody `render()` oraz kilka jej klas pochodnych. Stwórz klasę `Document` umożliwiającą wyrenderowanie dodanych elementów.
+Zaimplementuj klasy odpowiedzialne za tworzenie dokumentów w składni MarkDown. Stwórz klasę bazową `Element`
+zawierającą podstawową implementację metody `render()` oraz kilka jej klas pochodnych. Stwórz klasę `Document`
+umożliwiającą wyrenderowanie dodanych elementów.
 Przykład użycia:
 document = Document()
 document.add_element(HeaderElement('Header'))
@@ -14,16 +16,62 @@ Simple
 """
 
 class Element:
-    def __init__(self,text):
+
+    def __init__(self,text: str):
         self.text = text
 
     def render(self):
         return self.text
 
 class HeadElement(Element):
+    def __init__(self, text, level: int = 1):
+        self.text = text
+        self.level = level
+        return
+
     def render(self):
         text = super().render()
-        return text + "\n" +
+        if self.level == 1:
+            return text.upper() + "\n" + 30*'='
+        elif self.level == 2:
+            return text + "\n" + 30*'-'
+        else:
+            return text + "\n"
 
-e = HeadElement("Jakiś napis")
+class LinkElement(Element):
+    def __init__(self,text,adress):
+        self.text = text
+        self.adress = adress
+
+    def render(self):
+        return 'http://'+ self.text.lower() + self.adress[self.adress.index("."):]
+
+f = LinkElement('ABC', 'abc.com')
+
+class Document():
+    def __init__(self):
+        self.text = ""
+
+    def render(self):
+        return self.text
+
+
+def document_add_element(self):
+    return self.render()
+
+document = Document()
+
+print(document_add_element(HeadElement('Netflix',2)))
+print(document_add_element(Element('Nieograniczona oferta filmów, seriali, programów i nie tylko. Oglądaj wszędzie. Anuluj w każdej chwili.')))
+print(document_add_element(LinkElement('Netflix', 'netflix.org')))
+
+print("\n", 60*'*')
+
+e = HeadElement(f"To jest nagłówek")
 print(e.render())
+e = HeadElement(f"To jest nagłówek",2)
+print(e.render())
+e = HeadElement(f"To jest nagłówek",3)
+print(e.render())
+
+print(f.render())
