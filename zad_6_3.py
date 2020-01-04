21189215ebc31ffc12023da2a4dc0144f79f7e93
TOMASZPLONKA <57827736+TOMASZPLONKA@users.noreply.github.com>
2020-01-04 16:56:07 +0100

    Add files via upload

---
+++ zad_6_3.py
@@ -0,0 +1,6 @@
+"""
+Zadanie  3Napisz program wczytujący listę adresów email z pliku i tworzący nowy plik z odfiltrowaną zawartością.
+Wejściowy plik może zawierać duplikaty adresów, ten sam adres pisany różną wielkością liter, linie zawierające białe znaki oraz
+błędne adresy email (brak znaku `@` lub występuje on wiele razy). Wynikowy plik powinien zawierać unikalne, posortowane, poprawne adresy email.
+Przykład użycia:$ python clean_emails.py emails.txt cleaned_emails.txt"
+"""
